f290d4fbfa9075eace6a96f8f237cc62814bf64b
muskanpateriya <muskanpateriya04@gmail.com>
2023-07-05 21:47:28 +0530

    updated

--- springBootLearning/src/main/java/com/learning/ProjectLearningApplication.java
+++ springBootLearning/src/main/java/com/learning/ProjectLearningApplication.java
@@ -2,8 +2,10 @@ package com.learning;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 //main
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ProjectLearningApplication {
 
 	public static void main(String[] args) {
